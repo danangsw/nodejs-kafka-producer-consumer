dfd06641a7a1126631fa872f3ed27b4b096a2c42
danang.wantara <danangsukma@gmail.com>
2021-09-05 16:22:37 +0700

    realtime chart in FE

--- realtime-charts/src/App.js
+++ realtime-charts/src/App.js
@@ -1,26 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+import { Subscription } from 'react-apollo';
+import gql from 'graphql-tag';
+import moment from 'moment';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Sudah di-Edit <code>src/App.js</code> and save to reload.
-          Mantap!.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const TWENTY_MIN_TEMP_SUBSCRIPTION= gql`
+  subscription {
+    last_20_min_temp(
+      order_by: {
+        five_sec_interval: asc
+      }
+      where: {
+        location: {
+          _eq: "London"
+        }
+      }
+    ) {
+      five_sec_interval
+      location
+      max_temp
+    }
+  }
+`
+
+class App extends Component {
+  render() {
+    return(
+      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '20px'}}>
+      <Subscription subscription={TWENTY_MIN_TEMP_SUBSCRIPTION}>
+        {
+          ({data, error, loading}) => {
+            if (error) {
+              console.error(error);
+              return "Error";
+            }
+            if (loading) {
+              return "Loading";
+            }
+            
+            let chartJSData = {
+              labels: [],
+              datasets: [{
+                label: "Max temperature every five seconds",
+                data: [],
+                pointBackgroundColor: [],
+                borderColor: 'brown',
+                fill: false
+              }]
+            };
+
+            data.last_20_min_temp.forEach((item) => {
+              const humanReadableTime = moment(item.five_sec_interval).format('LTS');
+              chartJSData.labels.push(humanReadableTime);
+              chartJSData.datasets[0].data.push(item.max_temp);
+              chartJSData.datasets[0].pointBackgroundColor.push('brown');
+            });
+
+            return (
+              <Line
+                data={chartJSData}
+                options={{
+                  animation: {duration: 0},
+                  scales: { yAxes: [{ticks: { min: 5, max: 20 }}]}
+                }}
+              />
+            );
+          }
+        }
+      </Subscription>
     </div>
-  );
-}
+    );
+  }
+};
 
 export default App;
--- realtime-charts/src/index.js
+++ realtime-charts/src/index.js
@@ -2,16 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { WebSocketLink } from 'apollo-link-ws';
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+// Create a WebSocket link:
+const link = new WebSocketLink({
+  uri: `ws://localhost:8282/v1alpha1/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      headers: {
+        "x-hasura-admin-secret": "mylongsecretkey"
+      }
+    }
+  }
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  ),
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
